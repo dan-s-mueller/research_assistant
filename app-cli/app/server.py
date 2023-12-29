65a194dd1d288b7cebf25ca44ec738d21ff27473
dsmueller3760 <dsm@danmueller.pro>
2023-12-29 15:10:22 -0500

    Add new files and update existing files

---
+++ app-cli/app/server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv(),override=True)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+# Edit this to add the chain you want to add
+from research_assistant import chain as research_assistant_chain
+add_routes(app, research_assistant_chain, path="/research-assistant")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
